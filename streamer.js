8dafc10846aa86e2734469df3f0871b78c9f0f02
TJ <tj.abdoullah@gmail.com>
2023-03-11 07:04:53 +0100

    path corrected

--- streamer.js
+++ streamer.js
@@ -4,8 +4,8 @@ var handler = function (context) {
   context
     .onEvent("variables")
     .then(async function (vars) {
-      await context.streamFile("/voices/insurance");
-      return context.streamFile("/voices/insurance");
+      await context.streamFile("/home/ubuntu/voiccy-telephony/voices/insurance");
+      return context.streamFile("/home/ubuntu/voiccy-telephony/voices/insurance");
     })
     .then(function (result) {
       return context.end();
