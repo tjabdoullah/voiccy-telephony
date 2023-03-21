50083b93b279162be2cdc79cbaa73630d055573b
TJ <tj.abdoullah@gmail.com>
2023-03-21 18:32:54 +0000

    Voices

--- streamer.js
+++ streamer.js
@@ -5,11 +5,11 @@ var handler = function (context) {
     .onEvent("variables")
     .then(async function (vars) {
       await context.streamFile(
-        "voices/insurance_about_to_expire_1_to_know_more_2_to_oup_out"
+        "voiccy/insurance_about_to_expire_1_to_know_more_2_to_oup_out"
       );
-      await context.streamFile("voices/one_of_our_agents_will_call_you_soon");
-      await context.streamFile("voices/optout_received");
-      await context.streamFile("voices/thank_you_bye");
+      await context.streamFile("voiccy/one_of_our_agents_will_call_you_soon");
+      await context.streamFile("voiccy/optout_received");
+      await context.streamFile("voiccy/thank_you_bye");
     })
     .then(function (result) {
       return context.end();
