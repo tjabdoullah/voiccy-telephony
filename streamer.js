f4da4027796837b30b6933b634929ae333e4d422
TJ <tj.abdoullah@gmail.com>
2023-03-21 18:11:05 +0000

    audio files

--- streamer.js
+++ streamer.js
@@ -4,8 +4,12 @@ var handler = function (context) {
   context
     .onEvent("variables")
     .then(async function (vars) {
-      await context.streamFile("/home/ubuntu/voiccy-telephony/voices/insurance");
-      return context.streamFile("/home/ubuntu/voiccy-telephony/voices/insurance");
+      await context.streamFile(
+        "voices/insurance_about_to_expire_1_to_know_more_2_to_oup_out"
+      );
+      await context.streamFile("voices/one_of_our_agents_will_call_you_soon");
+      await context.streamFile("voices/optout_received");
+      await context.streamFile("voices/thank_you_bye");
     })
     .then(function (result) {
       return context.end();
